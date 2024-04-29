8404d30df3d9d6ac5809e51e453685eb330f6087
asyifa-mg <asyifamaharanigustina12@gmail.com>
2024-04-29 13:21:34 +0700

    backend

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,13 @@ import fs from "node:fs/promises";
 
 import bodyParser from "body-parser";
 import express from "express";
-import Films from "../src/components/Films";
 
 const app = express();
 
 app.use(express.static("images"));
 app.use(bodyParser.json());
 
+// CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // allow all domains
   res.setHeader("Access-Control-Allow-Methods", "GET, PUT");
@@ -17,31 +17,31 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/listfilms", async (req, res) => {
+app.get("/films", async (req, res) => {
   const fileContent = await fs.readFile("./data/films.json");
 
-  const filmData = JSON.parse(fileContent);
+  const filmsData = JSON.parse(fileContent);
 
-  res.status(200).json({ films: filmData });
+  res.status(200).json({ films: filmsData });
 });
 
-app.get("/add-listFilm", async (req, res) => {
-  const fileContent = await fs.readFile("./data/add-listFilm.json");
+app.get("/user-places", async (req, res) => {
+  const fileContent = await fs.readFile("./data/user-places.json");
 
   const films = JSON.parse(fileContent);
 
   res.status(200).json({ films });
 });
 
-app.put("/add-listFilm", async (req, res) => {
-  const films = req.body.places;
+app.put("/user-places", async (req, res) => {
+  const films = req.body.films;
 
-  await fs.writeFile("./data/add-listFilm.json", JSON.stringify(films));
+  await fs.writeFile("./data/user-places.json", JSON.stringify(films));
 
-  res.status(200).json({ message: "List Film Updated!" });
+  res.status(200).json({ message: "Lis Fim Updated!" });
 });
 
-//404;
+// 404
 app.use((req, res, next) => {
   if (req.method === "OPTIONS") {
     return next();
@@ -49,4 +49,4 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "404 - Not Found" });
 });
 
-app.listen(5000, () => console.log("Server up and running..."));
+app.listen(3000);
